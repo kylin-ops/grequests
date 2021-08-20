9530ddf1b951f954decc49c65b891d13bea7d4ad
刘清 <liuqing_mail@126.com>
2021-08-20 23:07:09 +0800

    增加文件加载功能

--- requests.go
+++ requests.go
@@ -3,9 +3,14 @@ package grequests
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"path"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -16,12 +21,18 @@ type Header map[string]string
 type Data map[string]interface{}
 type Param map[string]string
 
+type baseAuth struct {
+	UserName string
+	Password string
+}
+
 type RequestOptions struct {
-	Header  Header
-	Data    Data
-	Json    bool
-	Params  Param
-	Timeout time.Duration
+	Header   Header
+	Data     Data
+	Json     bool
+	Params   Param
+	Timeout  time.Duration
+	BashAuth baseAuth
 }
 
 func request(url, method string, options ...*RequestOptions) (resp *Response, err error) {
@@ -51,6 +62,9 @@ func request(url, method string, options ...*RequestOptions) (resp *Response, er
 	if option.Json {
 		r.Header.Set("Content-Type", "application/json")
 	}
+	if option.BashAuth.UserName != "" && option.BashAuth.Password != "" {
+		r.SetBasicAuth(option.BashAuth.UserName, option.BashAuth.Password)
+	}
 	response.Response, err = client.Do(r)
 	return &response, err
 }
@@ -77,6 +91,68 @@ func Delete(url string, options ...*RequestOptions) (resp *Response, err error) 
 
 func Head(url string, options ...*RequestOptions) (resp *Response, err error) {
 	return request(url, "HEAD", options...)
+}
+
+func destValidator(dest, filename string) (destFile string) {
+	if dest == "" {
+		dest = filename
+	} else {
+		f, err := os.Stat(dest)
+		if err == nil {
+			if f.IsDir() {
+				fmt.Println("a")
+				dest = path.Join(dest, filename)
+			} else {
+				dest = dest + strconv.Itoa(int(time.Now().Unix()))
+			}
+		}
+	}
+	return dest
+}
+
+// dest=""时，文件下载在本地
+func DownloadFile(url, dest string) error {
+	var buf = make([]byte, 32*1024)
+	var written int
+	fileName := path.Base(url)
+	req, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer req.Body.Close()
+	if req.Body == nil {
+		return errors.New("下载的类容为nil")
+	}
+	fsize, err := strconv.ParseInt(req.Header.Get("Content-Length"), 10, 32)
+	destFile := destValidator(dest, fileName)
+	destFileTemp := destFile + ".tmp"
+	f, err := os.Create(destFileTemp)
+	if err != nil {
+		return nil
+	}
+	defer func() {
+		f.Sync()
+		f.Close()
+		err = os.Rename(destFileTemp, destFile)
+	}()
+	for {
+		nr, err := req.Body.Read(buf)
+		written += nr
+		if err != nil {
+			if err == io.EOF {
+				if fsize != int64(written) {
+					return errors.New("下载文件大小不一致")
+				}
+				return nil
+			}
+			return err
+		}
+		if nr > 0 {
+			if _, er := f.Write(buf[0:nr]); er != nil {
+				return er
+			}
+		}
+	}
 }
 
 type Response struct {
